b703c49443a9a76a5b409c2746e01361e4eaf79a
Mateusz Jarzębowski-Bownik <mateuszjarzebowskibownik@Mateuszs-MacBook-Pro.local>
2024-10-10 09:13:48 +0200

    feat: another console.log (sic!)

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 console.log('Hello World');
 
 console.log("I'm not going to commit this, instead I'm just going to cz bump the version");
+
+console.log("Another one not commited, let's just see where it gets me xD");
