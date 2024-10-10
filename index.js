106a55d5acc938e9cd7c6cc857b974e7796c16d0
Mateusz Jarzębowski-Bownik <mateuszjarzebowskibownik@Mateuszs-MacBook-Pro.local>
2024-10-10 16:22:25 +0200

    bump: version 0.3.0 → 0.4.0

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-console.log('Hello World');
+console.log("Hello World");
 
-console.log("I'm not going to commit this, instead I'm just going to cz bump the version");
+console.log(
+  "I'm not going to commit this, instead I'm just going to cz bump the version"
+);
 
 console.log("Another one not commited, let's just see where it gets me xD");
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
