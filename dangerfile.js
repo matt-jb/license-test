276130ead76b77dbedf253d97713593727a886a5
Mateusz Jarzębowski-Bownik <mateuszjarzebowskibownik@Mateuszs-MacBook-Pro.local>
2024-10-10 08:51:30 +0200

    chore(package.json): add deps

---
+++ dangerfile.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-unresolved, no-undef, import/no-extraneous-dependencies */
+const licenseAuditor = require('@brainhubeu/license-auditor');
+
+const whitelist = require('./license/whitelist');
+const blacklist = require('./license/blacklist');
+const modules = require('./license/modules');
+
+licenseAuditor({
+  whitelistedLicenses: whitelist,
+  blacklistedLicenses: blacklist,
+  whitelistedModules: modules,
+  projectPath: process.env.PROJECT_PATH,
+  ciManager: {
+    warn,
+    fail,
+  },
+});
